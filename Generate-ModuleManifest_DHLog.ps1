70b3674aa6272fe1486ea1ce146358b6aebaba16
Darren Hollinrake <hollinrakedp@gmail.com>
2022-03-19 06:42:20 -0500

    Update Generate-ModuleManifest_DHLog.ps1

--- Generate-ModuleManifest_DHLog.ps1
+++ Generate-ModuleManifest_DHLog.ps1
@@ -1,11 +1,19 @@
+$ModuleName = $PSScriptRoot | Split-Path -Leaf
+$FunctionsToExport = @( (Get-ChildItem -Path $PSScriptRoot\$ModuleName\Public\*.ps1).BaseName )
+
 $manifest = @{
-    Path              = '.\DHLog\DHLog.psd1'
-    RootModule        = 'DHLog.psm1' 
+    Path              = "$PSScriptRoot\$ModuleName\$ModuleName.psd1"
+    RootModule        = "$ModuleName.psm1"
     Author            = 'Darren Hollinrake'
     Company           = 'Darren Hollinrake'
     Description       = 'Basic PowerShell Logging Module'
     ModuleVersion     = '1.1'
-    GUID              = 'a66fb643-d3cf-45b6-8205-ba386bd44bcb'
-    FunctionsToExport = @('Write-LogEntry')
+    FunctionsToExport = $FunctionsToExport
 }
-New-ModuleManifest @manifest
+
+if (Test-Path $manifest.Path){
+    Update-ModuleManifest @manifest
+}
+else {
+    New-ModuleManifest @manifest
+}
