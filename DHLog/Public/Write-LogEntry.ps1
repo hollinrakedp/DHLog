3b62eb652cb3982c6eccdfa532caf6cc7070a93b
Darren Hollinrake <50473419+hollinrakedp@users.noreply.github.com>
2024-10-23 20:22:58 -0500

    Fix description

--- DHLog/Public/Write-LogEntry.ps1
+++ DHLog/Public/Write-LogEntry.ps1
@@ -4,7 +4,7 @@ function Write-LogEntry {
     Writes a time-stamped message to a log file.
 
     .DESCRIPTION
-    This function adds more robust logging functionality for other scripts and functions. Each log entry is composed of three parts: timestamp, log level, and the message. The timestamp is in the following format: "yyyy-MM-dd HH:mm:ss:fff". There are three (5) log levels: ERROR, WARN, INFO, DEBUG, VERBOSE. Each of these direct output to a corresponding stream as well as to the log. (ERROR to the Error stream, WARN to the Warning stream, INFO to the Verbose stream, DEBUG to the Debug stream, VERBOSE to the Verbose stream).
+    This function adds more robust logging functionality for other scripts and functions. Each log entry is composed of three parts: timestamp, log level, and the message. The timestamp is in the following format: "yyyy-MM-dd HH:mm:ss:fff". There are five (5) log levels: ERROR, WARN, INFO, DEBUG, VERBOSE. Each of these direct output to a corresponding stream as well as to the log. (ERROR to the Error stream, WARN to the Warning stream, INFO to the Verbose stream, DEBUG to the Debug stream, VERBOSE to the Verbose stream).
 
     Example Entry
     -------------
